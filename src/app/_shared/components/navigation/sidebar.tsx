1d0ffcdfa78850d91d84add125540096437ffba9
Andres Velasco <velasco.andrs@gmail.com>
2024-02-28 00:14:36 -0500

    ⚡️ setup CI

--- src/app/_shared/components/navigation/sidebar.tsx
+++ src/app/_shared/components/navigation/sidebar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Github } from 'lucide-react'
